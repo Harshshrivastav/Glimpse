3c1845f624cac686eea52369d84c5bdf9a9ed56c
Harsh Shrivastava <99605597+Harshshrivastav@users.noreply.github.com>
2024-07-29 14:58:57 +0530

    Delete server/key.js

--- server/key.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGOURI: "mongodb+srv://harshshrivastava:Shrivastava096@cluster0.svvco9t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    JWT_SECRET: "bkuzdnnnsdjffbwdveargh",
-}
